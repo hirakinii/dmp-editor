ad8940087fd4d9c38f0ee3b2b477e4305303ac08
hirakinii <hiraki@nii.ac.jp>
2026-03-24 13:33:55 +0900

    feat: add init of i18n #55

---
+++ src/i18n/index.ts
@@ -0,0 +1,45 @@
+import i18n from "i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
+import { initReactI18next } from "react-i18next"
+
+import enCommon from "./locales/en/common.json"
+import enEditProject from "./locales/en/editProject.json"
+import enHome from "./locales/en/home.json"
+import enStatus from "./locales/en/status.json"
+import jaCommon from "./locales/ja/common.json"
+import jaEditProject from "./locales/ja/editProject.json"
+import jaHome from "./locales/ja/home.json"
+import jaStatus from "./locales/ja/status.json"
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ["ja", "en"],
+    fallbackLng: "ja",
+    defaultNS: "common",
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+      lookupLocalStorage: "i18nextLng",
+    },
+    resources: {
+      ja: {
+        common: jaCommon,
+        home: jaHome,
+        editProject: jaEditProject,
+        status: jaStatus,
+      },
+      en: {
+        common: enCommon,
+        home: enHome,
+        editProject: enEditProject,
+        status: enStatus,
+      },
+    },
+    interpolation: {
+      escapeValue: false, // React handles XSS
+    },
+  })
+
+export default i18n
